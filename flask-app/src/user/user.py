1d5bc8f2e4acf9a89309a1b1b5cdabc9db11f3b2
nicole-contreras <nicolecontreras9@icloud.com>
2023-12-06 04:57:36 -0500

    POST Conversation

--- flask-app/src/user/user.py
+++ flask-app/src/user/user.py
@@ -26,29 +26,6 @@ def getUserInfo(userID):
     return jsonify(json_data)
 
 
-@user.route('/<userIDFrom>/conversation/<userIDTo>', methods=['GET'])
-def get_conversation(userIDFrom, userIDTo):
-    query = '''
-        SELECT conversation_history
-        FROM conversation
-        WHERE (to_user_id = {} AND from_user_id = {})
-        OR (to_user_id = {} AND from_user_id = {});
-    '''.format(userIDTo, userIDFrom, userIDFrom, userIDTo)
-    
-    cursor = db.get_db().cursor()
-    cursor.execute(query) # store results in cursor
-
-    column_headers = [x[0] for x in cursor.description]
-
-    json_data = []
-
-    theData = cursor.fetchall()
-
-    for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
-
-    return jsonify(json_data)
-
 
 @user.route('/<userIDFrom>/conversation/<userIDTo>', methods=['GET'])
 def get_conversation(userIDFrom, userIDTo):
@@ -75,24 +52,30 @@ def get_conversation(userIDFrom, userIDTo):
 
 @user.route('/<userIDFrom>/conversation/<userIDTo>', methods=['POST'])
 def add_message_to_conversation(userIDFrom, userIDTo):
+    
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
 
-    message_data = request.json  
+    #extracting the variable
+    conversation_history = the_data
 
-    # Add the new message to the conversation
-    query = '''
-        INSERT INTO conversation (from_user_id, to_user_id, message)
-        VALUES (%s, %s, %s);
-    '''
-    values = (userIDFrom, userIDTo, message_data['message'])  
+    query = 'INSERT INTO conversation (to_user_id, from_user_id, conversation_history) VALUES ("'
+    query += str(userIDTo) + '", "'
+    query += str(userIDFrom) + '", "'
+    query += (conversation_history) + '")'
 
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
     cursor = db.get_db().cursor()
-    cursor.execute(query, values)
+    cursor.execute(query)
     db.get_db().commit()
+    
+    return 'Success!'
 
-    return jsonify({"message": "Message added successfully"})
-
-@user.route('/additem', methods=['POST'])
-def user_sell_new_item():
+@user.route('/<sellerID>/additem', methods=['POST'])
+def user_sell_new_item(sellerID):
     
     # collecting data from the request object 
     the_data = request.json
@@ -107,7 +90,7 @@ def user_sell_new_item():
     query += category + '", "'
     query += str(price) + '", "'
     query += size + '", "'
-    query += str(1) + '")'
+    query += str(sellerID) + '")'
 
 
     current_app.logger.info(query)
